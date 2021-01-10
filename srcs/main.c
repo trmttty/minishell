cd6a713893a9c8923bac7f651350bc42b6879b37
trmttty <tatsuya.tarumoto@gmail.com>
2021-01-10 21:08:38 +0900

    add pipe

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 18:38:26 by ttarumot          #+#    #+#             */
-/*   Updated: 2021/01/07 21:47:23 by ttarumot         ###   ########.fr       */
+/*   Updated: 2021/01/10 21:07:50 by ttarumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,45 @@ int		launch(char **args) {
     return (1);
 }
 
+int		launch_pipe(char **args) {
+    pid_t	pid;
+	pid_t	wpid;
+    int		status;
+
+    pid = fork();
+    if (pid == 0) {
+	    int		fd2[2];
+	    pid_t	pid2;
+
+	    pipe(fd2);
+	    if ((pid2 = fork()) == 0) {
+	        dup2(fd2[1], 1);
+	        close(fd2[0]); close(fd2[1]);
+	        execlp(args[0], args[0], (char*)NULL);
+	    }
+	    dup2(fd2[0], 0);
+	    close(fd2[0]);
+		close(fd2[1]);
+	    execlp(args[2], args[2], (char*)NULL);
+
+    } else if (pid < 0) {
+        // フォークでエラー
+        perror("lsh");
+    } else {
+        // 親プロセス
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    
+    return (1);
+}
+
+
 int		execute(char **args, t_list **env_lst)
 {
+	if (ft_tabsize(args) == 3 && ft_strcmp(args[1], "|") == 0)
+		return (launch_pipe(args));
 	if (ft_strcmp(args[0], "echo") == 0)
 		return (ft_echo(&args[1], env_lst));
 	if (ft_strcmp(args[0], "cd") == 0)
