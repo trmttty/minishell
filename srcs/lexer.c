b98c4f659f76f22c6d9d7ca6dd375f656056b470
trmttty <tatsuya.tarumoto@gmail.com>
2021-01-18 15:08:43 +0900

    modify lexer

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,7 +6,7 @@
 /*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 12:26:55 by ttarumot          #+#    #+#             */
-/*   Updated: 2021/01/18 12:26:59 by ttarumot         ###   ########.fr       */
+/*   Updated: 2021/01/18 15:08:03 by ttarumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,115 +18,102 @@
 #include <ctype.h>
 #include <stdio.h>
 
-
-t_lexer* init_lexer(char* contents)
+t_lexer*    init_lexer(char* contents)
 {
-    t_lexer* lexer;
-    
-    lexer = calloc(1, sizeof(t_lexer));
-    lexer->contents = contents;
-    lexer->i = 0;
-    lexer->c = contents[lexer->i];
-
-    return lexer;
+	t_lexer* lexer;
+	
+	lexer = ft_calloc(1, sizeof(t_lexer));
+	lexer->contents = contents;
+	lexer->i = 0;
+	lexer->c = contents[lexer->i];
+	return (lexer);
 }
 
-void lexer_advance(t_lexer* lexer)
+void        lexer_advance(t_lexer* lexer)
 {
-    if (lexer->c != '\0' && lexer->i < strlen(lexer->contents))
-    {
-        lexer->i += 1;
-        lexer->c = lexer->contents[lexer->i];
-    }
+	if (lexer->c != '\0' && lexer->i < ft_strlen(lexer->contents))
+	{
+		lexer->i += 1;
+		lexer->c = lexer->contents[lexer->i];
+	}
 }
 
 // whitespace?????
-void lexer_skip_whitespace(t_lexer* lexer)
+void        lexer_skip_whitespace(t_lexer* lexer)
 {
-    while (lexer->c == ' ' || lexer->c == '\n')
-    {
-        lexer_advance(lexer);
-    }
+	while (lexer->c == ' ' || lexer->c == '\n')
+	{
+		lexer_advance(lexer);
+	}
 }
 
-t_token* lexer_get_next_token(t_lexer* lexer)
+t_token*    lexer_get_next_token(t_lexer* lexer)
 {
-    while (lexer->c != '\0' && lexer->i < strlen(lexer->contents))
-    {
-        if (lexer->c == ' ' || lexer->c == 10)
-            lexer_skip_whitespace(lexer);
-
-        if (isalnum(lexer->c) || lexer->c == '-')
-            return (lexer_collect_id(lexer));
-
-        if (lexer->c == '"')
-            return (lexer_collect_string(lexer));
-
-        if (ft_strchr(";()<>|$", lexer->c))
-            return (lexer_advance_with_token(lexer, init_token(TK_RESERVED, lexer_get_current_char_as_string(lexer))));
-    }
-    return (NULL);
+	while (lexer->c != '\0' && lexer->i < ft_strlen(lexer->contents))
+	{
+		if (lexer->c == ' ' || lexer->c == '\n')
+			lexer_skip_whitespace(lexer);
+		if (lexer->c == '"')
+			return (lexer_collect_string(lexer));
+		if (ft_strchr(";()<>|", lexer->c))
+			return (lexer_advance_with_token(lexer, init_token(TK_RESERVED, lexer_get_current_char_as_string(lexer))));
+		return (lexer_collect_id(lexer));
+	}
+	return (NULL);
 }
 
-t_token* lexer_collect_string(t_lexer* lexer)
+t_token*    lexer_collect_string(t_lexer* lexer)
 {
-    char    *value;
-    char    *s;
-    size_t  size;
+	char    *value;
+	char    *s;
+	size_t  size;
 
-    lexer_advance(lexer);
-
-    value = calloc(1, sizeof(char));
-    value[0] = '\0';
-
-    while (lexer->c != '"')
-    {
-        s = lexer_get_current_char_as_string(lexer);
-        size = ft_strlen(value) + ft_strlen(s) + 1;
-        value = realloc(value, size * sizeof(char));
-        ft_strlcat(value, s, size);
-
-        lexer_advance(lexer);
-    }
-
-    lexer_advance(lexer);
-
-    return init_token(TK_CMD, value);
+	lexer_advance(lexer);
+	value = calloc(1, sizeof(char));
+	value[0] = '\0';
+	while (lexer->c != '"')
+	{
+		s = lexer_get_current_char_as_string(lexer);
+		size = ft_strlen(value) + ft_strlen(s) + 1;
+		value = realloc(value, size * sizeof(char));
+		ft_strlcat(value, s, size);
+		lexer_advance(lexer);
+	}
+	lexer_advance(lexer);
+	return (init_token(TK_CMD, value));
 }
 
-t_token* lexer_collect_id(t_lexer* lexer)
+t_token*    lexer_collect_id(t_lexer* lexer)
 {
-    char*   value;
-    char*   s;
-    size_t  size;
-    
-    value = ft_calloc(1, sizeof(char));
-    value[0] = '\0';
-    while (isalnum(lexer->c) || lexer->c == '-')
-    {
-        s = lexer_get_current_char_as_string(lexer);
-        size = ft_strlen(value) + ft_strlen(s) + 1;
-        value = realloc(value, size * sizeof(char));
-        ft_strlcat(value, s, size);
-
-        lexer_advance(lexer);
-    }
-
-    return init_token(TK_CMD, value);
+	char*   value;
+	char*   s;
+	size_t  size;
+	
+	value = ft_calloc(1, sizeof(char));
+	value[0] = '\0';
+	while (!(ft_strchr(";()<>|", lexer->c) || lexer->c == ' '))
+	{
+		s = lexer_get_current_char_as_string(lexer);
+		size = ft_strlen(value) + ft_strlen(s) + 1;
+		value = realloc(value, size * sizeof(char));
+		ft_strlcat(value, s, size);
+		lexer_advance(lexer);
+	}
+	return (init_token(TK_CMD, value));
 }
 
-t_token* lexer_advance_with_token(t_lexer* lexer, t_token* token)
+t_token*    lexer_advance_with_token(t_lexer* lexer, t_token* token)
 {
-    lexer_advance(lexer);
-
-    return token;
+	lexer_advance(lexer);
+	return (token);
 }
 
 char* lexer_get_current_char_as_string(t_lexer* lexer)
 {
-    char* str = ft_calloc(2, sizeof(char));
-    str[0] = lexer->c;
-    str[1] = '\0';
-
-    return str;
+	char*	str;
+	
+	str = ft_calloc(2, sizeof(char));
+	str[0] = lexer->c;
+	str[1] = '\0';
+	return (str);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 18:38:26 by ttarumot          #+#    #+#             */
-/*   Updated: 2021/01/18 12:49:08 by ttarumot         ###   ########.fr       */
+/*   Updated: 2021/01/18 14:38:17 by ttarumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ int		launch(char **args) {
 	pid_t	wpid;
     int		status;
 
-	// printf("%s\n", args[0]);
-	// printf("%s\n", args[1]);
     pid = fork();
     if (pid == 0) {
         // 子プロセス
@@ -43,117 +41,117 @@ int		launch(char **args) {
     return (1);
 }
 
-int		launch_pipe(char **args) {
-    pid_t	pid;
-	pid_t	wpid;
-    int		status;
+// int		launch_pipe(char **args) {
+//     pid_t	pid;
+// 	pid_t	wpid;
+//     int		status;
 
-    pid = fork();
-    if (pid == 0) {
-	    int		fd2[2];
-	    pid_t	pid2;
+//     pid = fork();
+//     if (pid == 0) {
+// 	    int		fd2[2];
+// 	    pid_t	pid2;
 
-	    pipe(fd2);
-	    if ((pid2 = fork()) == 0) {
-	        dup2(fd2[1], 1);
-	        close(fd2[0]); close(fd2[1]);
-	        execlp(args[0], args[0], (char*)NULL);
-	    }
-	    dup2(fd2[0], 0);
-	    close(fd2[0]);
-		close(fd2[1]);
-	    execlp(args[2], args[2], (char*)NULL);
+// 	    pipe(fd2);
+// 	    if ((pid2 = fork()) == 0) {
+// 	        dup2(fd2[1], 1);
+// 	        close(fd2[0]); close(fd2[1]);
+// 	        execlp(args[0], args[0], (char*)NULL);
+// 	    }
+// 	    dup2(fd2[0], 0);
+// 	    close(fd2[0]);
+// 		close(fd2[1]);
+// 	    execlp(args[2], args[2], (char*)NULL);
 
-    } else if (pid < 0) {
-        // フォークでエラー
-        perror("lsh");
-    } else {
-        // 親プロセス
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+//     } else if (pid < 0) {
+//         // フォークでエラー
+//         perror("lsh");
+//     } else {
+//         // 親プロセス
+//         do {
+//             wpid = waitpid(pid, &status, WUNTRACED);
+//         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+//     }
     
-    return (1);
-}
+//     return (1);
+// }
 
-int		launch_oredirect(char **args) {
-    pid_t	pid;
-	pid_t	wpid;
-    int		status;
+// int		launch_oredirect(char **args) {
+//     pid_t	pid;
+// 	pid_t	wpid;
+//     int		status;
 
-    pid = fork();
-    if (pid == 0) {
-		int	fd;
-	    fd = open (args[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-	    dup2 (fd, STDOUT_FILENO);
-		args[1] = NULL;
-	    execvp (args[0], &args[0]);
+//     pid = fork();
+//     if (pid == 0) {
+// 		int	fd;
+// 	    fd = open (args[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+// 	    dup2 (fd, STDOUT_FILENO);
+// 		args[1] = NULL;
+// 	    execvp (args[0], &args[0]);
 
-    } else if (pid < 0) {
-        // フォークでエラー
-        perror("lsh");
-    } else {
-        // 親プロセス
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+//     } else if (pid < 0) {
+//         // フォークでエラー
+//         perror("lsh");
+//     } else {
+//         // 親プロセス
+//         do {
+//             wpid = waitpid(pid, &status, WUNTRACED);
+//         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+//     }
     
-    return (1);
-}
+//     return (1);
+// }
 
-int		launch_iredirect(char **args) {
-    pid_t	pid;
-	pid_t	wpid;
-    int		status;
+// int		launch_iredirect(char **args) {
+//     pid_t	pid;
+// 	pid_t	wpid;
+//     int		status;
 
-    pid = fork();
-    if (pid == 0) {
-		int	fd;
-	    fd = open (args[2], O_RDONLY);
-	    dup2 (fd, STDIN_FILENO);
-		args[1] = NULL;
-	    execvp (args[0], &args[0]);
+//     pid = fork();
+//     if (pid == 0) {
+// 		int	fd;
+// 	    fd = open (args[2], O_RDONLY);
+// 	    dup2 (fd, STDIN_FILENO);
+// 		args[1] = NULL;
+// 	    execvp (args[0], &args[0]);
 
-    } else if (pid < 0) {
-        // フォークでエラー
-        perror("lsh");
-    } else {
-        // 親プロセス
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+//     } else if (pid < 0) {
+//         // フォークでエラー
+//         perror("lsh");
+//     } else {
+//         // 親プロセス
+//         do {
+//             wpid = waitpid(pid, &status, WUNTRACED);
+//         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+//     }
     
-    return (1);
-}
+//     return (1);
+// }
 
 
-int		execute(char **args, t_list **env_lst)
-{
-	if (ft_tabsize(args) == 3 && ft_strcmp(args[1], "|") == 0)
-		return (launch_pipe(args));
-	if (ft_tabsize(args) == 3 && ft_strcmp(args[1], ">") == 0)
-		return (launch_oredirect(args));
-	if (ft_tabsize(args) == 3 && ft_strcmp(args[1], "<") == 0)
-		return (launch_iredirect(args));
-	if (ft_strcmp(args[0], "echo") == 0)
-		return (ft_echo(&args[1], env_lst));
-	if (ft_strcmp(args[0], "cd") == 0)
-		return (ft_cd(&args[1], env_lst));
-	if (ft_strcmp(args[0], "export") == 0)
-		return (ft_export(&args[1], env_lst));
-	if (ft_strcmp(args[0], "unset") == 0)
-		return (ft_unset(&args[1], env_lst));
-	if (ft_strcmp(args[0], "pwd") == 0)
-		return (ft_pwd(&args[1], env_lst));
-	if (ft_strcmp(args[0], "env") == 0)
-		return (ft_env(&args[1], env_lst));
-	if (ft_strcmp(args[0], "exit") == 0)
-		return (ft_exit(&args[1], env_lst));
-	return launch(args);
-}
+// int		execute(char **args, t_list **env_lst)
+// {
+// 	if (ft_tabsize(args) == 3 && ft_strcmp(args[1], "|") == 0)
+// 		return (launch_pipe(args));
+// 	if (ft_tabsize(args) == 3 && ft_strcmp(args[1], ">") == 0)
+// 		return (launch_oredirect(args));
+// 	if (ft_tabsize(args) == 3 && ft_strcmp(args[1], "<") == 0)
+// 		return (launch_iredirect(args));
+// 	if (ft_strcmp(args[0], "echo") == 0)
+// 		return (ft_echo(&args[1], env_lst));
+// 	if (ft_strcmp(args[0], "cd") == 0)
+// 		return (ft_cd(&args[1], env_lst));
+// 	if (ft_strcmp(args[0], "export") == 0)
+// 		return (ft_export(&args[1], env_lst));
+// 	if (ft_strcmp(args[0], "unset") == 0)
+// 		return (ft_unset(&args[1], env_lst));
+// 	if (ft_strcmp(args[0], "pwd") == 0)
+// 		return (ft_pwd(&args[1], env_lst));
+// 	if (ft_strcmp(args[0], "env") == 0)
+// 		return (ft_env(&args[1], env_lst));
+// 	if (ft_strcmp(args[0], "exit") == 0)
+// 		return (ft_exit(&args[1], env_lst));
+// 	return launch(args);
+// }
 
 void	loop(t_list **env_lst)
 {
