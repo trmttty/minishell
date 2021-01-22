c61b938a0f38f32ea27031ce23d30b68b3f3f740
trmttty <tatsuya.tarumoto@gmail.com>
2021-01-23 00:33:08 +0900

    modify

--- srcs/pwd.c
+++ srcs/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 03:24:43 by ttarumot          #+#    #+#             */
-/*   Updated: 2021/01/22 22:24:18 by ttarumot         ###   ########.fr       */
+/*   Updated: 2021/01/23 00:32:32 by ttarumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ int		ft_pwd(char **args, t_list **env_lst)
     char    buf[MAXPATHLEN];
     char    *ret;
 
-    ft_putendl_fd("test", 1);
     if ((ret = getcwd(buf, MAXPATHLEN)) != NULL)
     {
         ft_putendl_fd(buf, 1);
-        ft_putendl_fd(ret, 1);
         return (1);
     }
     return (0);
