3d043e742d3193bbbddec0868f3fb5851607ac0b
trmttty <tatsuya.tarumoto@gmail.com>
2021-01-30 23:39:20 +0900

    merge

--- srcs/sort_cmd.c
+++ srcs/sort_cmd.c
@@ -1,6 +1,42 @@
 #include "minishell.h"
 #include <ctype.h>
 
+char	*move_cmd(char *str, int *i, int *argc)
+{
+	int		len;
+	char	*cmd;
+	char	*cut;
+	char	*tmp;
+
+	len = 0;
+	while (isspace(str[*i + len]))
+		len++;
+	while (str[*i + len] && !ft_strchr("<> ;|", str[*i + len]))
+		len++;
+	cmd = ft_substr(str, *i, len);
+	str[*i] = 0;
+	cut = ft_strjoin(str, &str[*i + len]);
+	free(str);
+	if (*argc == 0)
+		str = ft_strjoin(cmd, cut);
+	else
+	{
+		tmp = ft_substr(cut, argc, ft_strlen(cut) - *argc);
+		cut[*argc] = 0;
+		str = ft_strjoin(cut, cmd);
+		free(cut);
+		cut = ft_strjoin(str, tmp);
+		free(str);
+		str = ft_strdup(cut);
+		free(tmp);
+	}
+	*argc += len;
+	*i += len;
+	free(cmd);
+	free(cut);
+	return (str);
+}
+
 char	*sort_cmd(char *str)
 {
 	int		i;
@@ -41,7 +77,7 @@ char	*sort_cmd(char *str)
 			//リダイレクションの場合、ポインタを進める
 			while (isspace(str[i]))
 				i++;
-			while (!ft_strchr("<> ;|", str[i]))
+			while (str[i] && !ft_strchr("<> ;|", str[i]))
 			{
 				if (ft_strchr("\"\'", str[i]))
 				{
@@ -55,6 +91,7 @@ char	*sort_cmd(char *str)
 		}
 		else
 		{
+			// move_cmd(str, &i, &argc);
 			//コマンドを切り取り、前に入れ替える
 			int		len = 0;
 			while (isspace(str[i + len]))
