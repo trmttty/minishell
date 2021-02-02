e0634306447f1dc19228804b8f38218039814769
trmttty <tatsuya.tarumoto@gmail.com>
2021-02-02 18:48:38 +0900

    merge

---
+++ srcs/realloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   realloc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/01 21:15:41 by kazumanoda        #+#    #+#             */
+/*   Updated: 2021/02/02 18:32:50 by ttarumot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	void	*cpy;
+
+	cpy = ft_calloc(size, 1);
+	if (!cpy)
+		return (NULL);
+	ft_memcpy(cpy, ptr, size);
+    free(ptr);
+	return (cpy);
+}
