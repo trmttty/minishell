aa6d59480dead5b44a41d1dac89993aa2e5dfcfb
odkaz <one.destiny.japan@gmail.com>
2021-02-05 20:10:04 +0900

    exit: error adjustments

--- srcs/error.c
+++ srcs/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kazumanoda <kazumanoda@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 13:14:01 by ttarumot          #+#    #+#             */
-/*   Updated: 2021/02/04 22:54:55 by ttarumot         ###   ########.fr       */
+/*   Updated: 2021/02/05 20:05:09 by kazumanoda       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void	exit_status(char *commnad, char *arg, char *message, int code)
 {
-	ft_putendl_fd("minishell: ", 2);
+	ft_putstr_fd("minishell: ", 2);
 	if (commnad)
 	{
-		ft_putendl_fd(commnad, 2);
-		ft_putendl_fd(": ", 2);
+		ft_putstr_fd(commnad, 2);
+		ft_putstr_fd(": ", 2);
 	}
 	if (arg)
 	{
-		ft_putendl_fd(arg, 2);
-		ft_putendl_fd(": ", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": ", 2);
 	}
 	if (message)
 	{
-		ft_putendl_fd(message, 2);
+		ft_putstr_fd(message, 2);
 	}
-	ft_putendl_fd("\n", 2);
+	ft_putstr_fd("\n", 2);
 	exit(code);
 }
 
--- srcs/exit.c
+++ srcs/exit.c
@@ -3,14 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kazumanoda <kazumanoda@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 02:33:09 by ttarumot          #+#    #+#             */
-/*   Updated: 2021/02/04 15:45:35 by ttarumot         ###   ########.fr       */
+/*   Updated: 2021/02/05 20:06:20 by kazumanoda       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	is_wspace(char c)
+{
+	return (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' \
+			|| c == ' ');
+}
+
+static long	set_number(char *str, int sign)
+{
+	long		nb;
+	char	*ptr;
+
+	nb = 0;
+	ptr = str;
+	while (ft_isdigit(*str))
+	{
+		if (nb == 922337203685477580 && *str >= '8' && sign == 1)
+			exit_status("exit", ptr, "numeric argument required", 255);
+		if (nb > 922337203685477580 && sign == 1)
+			exit_status("exit", ptr, "numeric argument required", 1);
+		if (nb == 922337203685477580 && *str >= '9' && sign == -1)
+			exit_status("exit", ptr, "numeric argument required", 255);
+		if (nb > 922337203685477580 && sign == -1)
+			exit_status("exit", ptr, "numeric argument required", 255);
+		nb = nb * 10 + (*str++ - '0');
+	}
+	return (nb);
+}
+
+int			my_atoi(char *str)
+{
+	int		sign;
+
+	sign = 1;
+	while (is_wspace(*str))
+		str++;
+	if (*str == '+')
+		str++;
+	else if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	return ((int)(sign * set_number(str, sign)));
+}
 
 int		is_num_str(char *str)
 {
@@ -33,10 +78,13 @@ int		ft_exit(char **args)
 	if (*args)
 	{
 		if (!is_num_str(*args))
-			exit(255);
+		{
+			exit_status("exit", *args, "numeric argument required", 255);
+			// exit(255);
+		}
 		if (args[1])
 			return (error_status("exit", NULL, "too many arguments", 1));
-		rv = ft_atoi(*args);
+		rv = my_atoi(*args);
 		rv %= 256;
 		if (rv < 0)
 			rv += 256;
