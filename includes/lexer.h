91403c1587adac53ff07a28ed67e44477db111c8
trmttty <tatsuya.tarumoto@gmail.com>
2021-02-15 15:01:04 +0900

    norm

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 00:09:13 by ttarumot          #+#    #+#             */
-/*   Updated: 2021/02/15 13:06:19 by ttarumot         ###   ########.fr       */
+/*   Updated: 2021/02/15 15:00:57 by ttarumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,6 @@ int					ft_isescape(t_lexer *lexer);
 int					lexer_expand_env(t_lexer *lexer, char **value);
 void				update_contens(t_lexer *lexer, char *value);
 char				*lexer_get_exit_code(t_lexer *lexer);
-
-
 t_token				*lexer_loop_raw(t_lexer *lexer, char **value);
 char				**lexer_expand_command(char **commands);
 char				**lexer_expand_file_name(char **commands);
